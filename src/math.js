7b10150d58356dc6324c08b0ef2285b209947c29
giota-cliqz <panagiota@cliqz.com>
2016-05-31 11:02:34 +0200

    Add jsdoc comments

--- src/math.js
+++ src/math.js
@@ -1,7 +1,11 @@
+/** This is description of the add function. */
 function add(x, y) {
 	return x + y;
 }
 
+/** This is description of the multiply function */
 function multiply(x, y) {
 	return x * y;
 }
+
+
